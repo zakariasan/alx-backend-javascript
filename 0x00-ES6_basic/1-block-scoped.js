60b40a5eecf28f572c92a88cf663ea3e53c6209e
zakariasan <elitziko@gmail.com>
2024-04-23 12:47:28 +0100

    es6 basics mm

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,9 +3,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true; //es
+    const task = true; //esconst
     const task2 = false;
   }
-
   return [task, task2];
 }
