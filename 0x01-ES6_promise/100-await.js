99d8e7cceb1c1624492a7e4b9efbcb6e9a448ca4
zakariasan <elitziko@gmail.com>
2024-04-24 17:54:21 +0100

    await promises errors

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -2,7 +2,7 @@ import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
   try {
-    const [photos, users] = await Promise.all([uploadPhoto(), createUser()])
+    const [photos, users] = await Promise.all([uploadPhoto(), createUser()]);
     return {
       photo: photos,
       user: users
