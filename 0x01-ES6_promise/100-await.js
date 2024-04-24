16b88e903c59a1de9dbc86cf3cfb9f6b4d42d3b4
zakariasan <elitziko@gmail.com>
2024-04-24 17:48:24 +0100

    await promises

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,16 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  try {
+    const [photos, users] = await Promise.all([uploadPhoto(), createUser()])
+    return {
+      photo: photos,
+      user: users
+    };
+  } catch (err) {
+    return {
+      photo: null,
+      user: null
+    }
+  };
+}
