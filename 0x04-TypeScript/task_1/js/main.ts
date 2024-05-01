a97af3515b763de848a5f9972fd497fee74db705
zakariasan <elitziko@gmail.com>
2024-05-01 23:40:25 +0100

    typeScript

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,43 @@
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  readonly fullTimeEmployee: boolean;
+  readonly yearsOfExperience?: number;
+  readonly location: string;
+  [key: string]: any; // Allow any other attribute
+}
+
+class TeacherImpl implements Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  readonly fullTimeEmployee: boolean;
+  readonly yearsOfExperience?: number;
+  readonly location: string;
+  [key: string]: any;
+
+  constructor(firstName: string, lastName: string, fullTimeEmployee: boolean, location: string, otherAttributes: any) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.fullTimeEmployee = fullTimeEmployee;
+    this.location = location;
+    Object.assign(this, otherAttributes);
+  }
+}
+
+const teacher3: Teacher = new TeacherImpl('John', 'Doe', false, 'London', { contract: false });
+
+console.log(teacher3);
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
+
