2b1919c859db3e495724684460842e290dee5e03
zakariasan <elitziko@gmail.com>
2024-05-19 16:58:25 +0100

    fix: this._floors 6

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -3,7 +3,7 @@ import Building from './5-building';
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    this._floors = floors;
+    this.floors = floors;
   }
 
   evacuationWarningMessage() {
