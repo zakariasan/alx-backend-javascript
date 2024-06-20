2aa964c737664672f13e7ed07425e0246240982c
zakariasan <elitziko@gmail.com>
2024-06-20 16:31:22 +0100

    fix: http try

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,5 +1,5 @@
-const { expect } = require('chai');
-const calculateNumber = require('./2-calcul_chai');
+import { expect } from 'chai';
+import calculateNumber from './2-calcul_chai.mjs';
 
 describe('calculateNumber', function () {
   describe('SUM', function () {
@@ -7,8 +7,8 @@ describe('calculateNumber', function () {
       expect(calculateNumber('SUM', 1, 3)).to.equal(4);
     });
 
-    it('should return 5 when type is SUM, a is 1.2 and b is 3.7', function () {
-      expect(calculateNumber('SUM', 1.2, 3.7)).to.equal(5);
+    it('should return 6 when type is SUM, a is 1.4 and b is 4.5', function () {
+      expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
     });
 
     it('should return 6 when type is SUM, a is 1.5 and b is 3.7', function () {
@@ -21,8 +21,8 @@ describe('calculateNumber', function () {
       expect(calculateNumber('SUBTRACT', 1, 3)).to.equal(-2);
     });
 
-    it('should return -2 when type is SUBTRACT, a is 1.2 and b is 3.7', function () {
-      expect(calculateNumber('SUBTRACT', 1.2, 3.7)).to.equal(-2);
+    it('should return -4 when type is SUBTRACT, a is 1.4 and b is 4.5', function () {
+      expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
     });
 
     it('should return -2 when type is SUBTRACT, a is 1.5 and b is 3.7', function () {
@@ -35,12 +35,12 @@ describe('calculateNumber', function () {
       expect(calculateNumber('DIVIDE', 1, 4)).to.be.closeTo(0.25, 0.01);
     });
 
-    it('should return 0.2 when type is DIVIDE, a is 1.2 and b is 4.7', function () {
-      expect(calculateNumber('DIVIDE', 1.2, 4.7)).to.be.closeTo(0.2, 0.01);
+    it('should return 0.2 when type is DIVIDE, a is 1.4 and b is 4.5', function () {
+      expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.be.closeTo(0.2, 0.01);
     });
 
-    it('should return 0.33 when type is DIVIDE, a is 1.5 and b is 4.5', function () {
-      expect(calculateNumber('DIVIDE', 1.5, 4.5)).to.be.closeTo(0.33, 0.01);
+    it('should return Error when type is DIVIDE, a is 1.4 and b is 0', function () {
+      expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
     });
 
     it('should return Error when type is DIVIDE, a is 1 and b is 0', function () {
