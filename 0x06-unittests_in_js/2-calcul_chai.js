e2fff6483eb47b9edba388214d68994f4ca72ac4
zakariasan <elitziko@gmail.com>
2024-06-20 15:43:28 +0100

    fix: http try

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -1,10 +1,10 @@
 function calculateNumber(type, a, b) {
-  if (type === "SUM") {
+  if (type === 'SUM') {
     return Math.round(a) + Math.round(b);
-  } else if (type === "SUBTRACT") {
+  } else if (type === 'SUBTRACT') {
     return Math.round(a) - Math.round(b);
   } else if (type === "DIVIDE") {
-    if (b === 0) {
+    if (Math.round(b) === 0) {
       return "Error";
     }
     return Math.round(a) / Math.round(b);
--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,54 +1,54 @@
-const assert = require('assert');
-const calculateNumber = require('./1-calcul');
+const { expect } = require('chai');
+const calculateNumber = require('./2-calcul_chai');
 
 describe('calculateNumber', function () {
   describe('SUM', function () {
     it('should return 4 when type is SUM, a is 1 and b is 3', function () {
-      assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
+      expect(calculateNumber('SUM', 1, 3)).to.equal(4);
     });
 
     it('should return 5 when type is SUM, a is 1.2 and b is 3.7', function () {
-      assert.strictEqual(calculateNumber('SUM', 1.2, 3.7), 5);
+      expect(calculateNumber('SUM', 1.2, 3.7)).to.equal(5);
     });
 
     it('should return 6 when type is SUM, a is 1.5 and b is 3.7', function () {
-      assert.strictEqual(calculateNumber('SUM', 1.5, 3.7), 6);
+      expect(calculateNumber('SUM', 1.5, 3.7)).to.equal(6);
     });
   });
 
   describe('SUBTRACT', function () {
     it('should return -2 when type is SUBTRACT, a is 1 and b is 3', function () {
-      assert.strictEqual(calculateNumber('SUBTRACT', 1, 3), -2);
+      expect(calculateNumber('SUBTRACT', 1, 3)).to.equal(-2);
     });
 
     it('should return -2 when type is SUBTRACT, a is 1.2 and b is 3.7', function () {
-      assert.strictEqual(calculateNumber('SUBTRACT', 1.2, 3.7), -2);
+      expect(calculateNumber('SUBTRACT', 1.2, 3.7)).to.equal(-2);
     });
 
     it('should return -2 when type is SUBTRACT, a is 1.5 and b is 3.7', function () {
-      assert.strictEqual(calculateNumber('SUBTRACT', 1.5, 3.7), -2);
+      expect(calculateNumber('SUBTRACT', 1.5, 3.7)).to.equal(-2);
     });
   });
 
   describe('DIVIDE', function () {
     it('should return 0.25 when type is DIVIDE, a is 1 and b is 4', function () {
-      assert.strictEqual(calculateNumber('DIVIDE', 1, 4), 0.25);
+      expect(calculateNumber('DIVIDE', 1, 4)).to.be.closeTo(0.25, 0.01);
     });
 
     it('should return 0.2 when type is DIVIDE, a is 1.2 and b is 4.7', function () {
-      assert.strictEqual(calculateNumber('DIVIDE', 1.2, 4.7), 0.2);
+      expect(calculateNumber('DIVIDE', 1.2, 4.7)).to.be.closeTo(0.2, 0.01);
     });
 
     it('should return 0.33 when type is DIVIDE, a is 1.5 and b is 4.5', function () {
-      assert.strictEqual(calculateNumber('DIVIDE', 1.5, 4.5), 0.33);
+      expect(calculateNumber('DIVIDE', 1.5, 4.5)).to.be.closeTo(0.33, 0.01);
     });
 
     it('should return Error when type is DIVIDE, a is 1 and b is 0', function () {
-      assert.strictEqual(calculateNumber('DIVIDE', 1, 0), 'Error');
+      expect(calculateNumber('DIVIDE', 1, 0)).to.equal('Error');
     });
 
     it('should return Error when type is DIVIDE, a is 1 and b is 0.4', function () {
-      assert.strictEqual(calculateNumber('DIVIDE', 1, 0.4), 'Error');
+      expect(calculateNumber('DIVIDE', 1, 0.4)).to.equal('Error');
     });
   });
 });
