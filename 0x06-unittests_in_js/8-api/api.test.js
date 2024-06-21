841ddc53a090c0d2cf723b0f31bdeb86faf7deda
zakariasan <elitziko@gmail.com>
2024-06-21 03:26:22 +0100

    making change

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,28 +1,44 @@
 // api.test.js
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const { expect } = chai;
-const app = require('./api');  // Ensure the server is running before testing
+const request = require('request');
+const { expect } = require('chai');
 
-chai.use(chaiHttp);
+const API_URL = 'http://localhost:7865';
 
-describe('Index page', () => {
-  it('should return status code 200', (done) => {
-    chai.request(app)
-      .get('/')
-      .end((err, res) => {
-        expect(res).to.have.status(200);
-        done();
-      });
+describe('API integration test', () => {
+  it('GET / returns correct response', (done) => {
+    request.get(`${API_URL}/`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
+      done();
+    });
   });
 
-  it('should return the correct message', (done) => {
-    chai.request(app)
-      .get('/')
-      .end((err, res) => {
-        expect(res.text).to.equal('Welcome to the payment system');
-        done();
-      });
+  it('GET /available_payments returns correct response', (done) => {
+    request.get(`${API_URL}/available_payments`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      const expectedResponse = {
+        payment_methods: {
+          credit_cards: true,
+          paypal: false
+        }
+      };
+      expect(JSON.parse(body)).to.deep.equal(expectedResponse);
+      done();
+    });
+  });
+
+  it('POST /login returns correct response', (done) => {
+    const options = {
+      url: `${API_URL}/login`,
+      json: {
+        userName: 'Betty'
+      }
+    };
+    request.post(options, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome Betty');
+      done();
+    });
   });
 });
 
