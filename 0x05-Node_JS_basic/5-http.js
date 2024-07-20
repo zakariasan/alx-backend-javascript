7c4620ff66e84e65e4846a5c68a8f73e6cbae2b1
zakariasan <elitziko@gmail.com>
2024-07-20 17:54:58 +0100

    http work

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,38 +1,107 @@
 const http = require('http');
-const { countStudents } = require('./3-read_file_async');
 const fs = require('fs');
 
-const PORT = 1245;
+const SERVER_PORT = 1245;
+const SERVER_HOST = 'localhost';
+const server = http.createServer();
+const DATABASE_FILE = process.argv.length > 2 ? process.argv[2] : '';
 
-const app = http.createServer((req, res) => {
-  const { method, url } = req;
+const readStudentData = (filePath) => new Promise((resolve, reject) => {
+  if (!filePath) {
+    reject(new Error('Cannot load the database'));
+  }
+  if (filePath) {
+    fs.readFile(filePath, (error, fileData) => {
+      if (error) {
+        reject(new Error('Cannot load the database'));
+      }
+      if (fileData) {
+        const output = [];
+        const lines = fileData.toString('utf-8').trim().split('\n');
+        const studentClasses = {};
+        const headers = lines[0].split(',');
+        const studentAttributes = headers.slice(0, headers.length - 1);
 
-  if (method === 'GET' && url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello Holberton School!');
-  } else if (method === 'GET' && url === '/students') {
-    const databasePath = process.argv.length > 2 ? process.argv[2] : '';
-    countStudents(databasePath)
-      .then((report) => {
-        const responseText = [
-          'This is the list of our students',
-          report
-        ].join('\n');
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end(responseText);
-      })
-      .catch((err) => {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end(err instanceof Error ? err.message : err.toString());
-      });
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/html' });
-    res.end('<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><title>Error</title></head><body><pre>Cannot GET ' + url + '</pre></body></html>');
+        for (const line of lines.slice(1)) {
+          const studentDetails = line.split(',');
+          const studentInfo = studentDetails.slice(0, studentDetails.length - 1);
+          const studentClass = studentDetails[studentDetails.length - 1];
+          if (!Object.keys(studentClasses).includes(studentClass)) {
+            studentClasses[studentClass] = [];
+          }
+          const studentEntries = studentAttributes.map((attribute, index) => [
+            attribute,
+            studentInfo[index],
+          ]);
+          studentClasses[studentClass].push(Object.fromEntries(studentEntries));
+        }
+
+        const totalStudents = Object.values(studentClasses).reduce(
+          (prev, curr) => (prev || []).length + curr.length,
+        );
+        output.push(`Number of students: ${totalStudents}`);
+        for (const [studentClass, students] of Object.entries(studentClasses)) {
+          output.push([
+            `Number of students in ${studentClass}: ${students.length}.`,
+            'List:',
+            students.map((student) => student.firstname).join(', '),
+          ].join(' '));
+        }
+        resolve(output.join('\n'));
+      }
+    });
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
+const ROUTE_HANDLERS = [
+  {
+    path: '/',
+    handler(_, response) {
+      const message = 'Hello Holberton School!';
+
+      response.setHeader('Content-Type', 'text/plain');
+      response.setHeader('Content-Length', message.length);
+      response.statusCode = 200;
+      response.write(Buffer.from(message));
+    },
+  },
+  {
+    path: '/students',
+    handler(_, response) {
+      const output = ['This is the list of our students'];
+
+      readStudentData(DATABASE_FILE)
+        .then((report) => {
+          output.push(report);
+          const message = output.join('\n');
+          response.setHeader('Content-Type', 'text/plain');
+          response.setHeader('Content-Length', message.length);
+          response.statusCode = 200;
+          response.write(Buffer.from(message));
+        })
+        .catch((err) => {
+          output.push(err instanceof Error ? err.message : err.toString());
+          const message = output.join('\n');
+          response.setHeader('Content-Type', 'text/plain');
+          response.setHeader('Content-Length', message.length);
+          response.statusCode = 200;
+          response.write(Buffer.from(message));
+        });
+    },
+  },
+];
+
+server.on('request', (req, res) => {
+  for (const routeHandler of ROUTE_HANDLERS) {
+    if (routeHandler.path === req.url) {
+      routeHandler.handler(req, res);
+      break;
+    }
+  }
 });
 
-module.exports = app;
+server.listen(SERVER_PORT, SERVER_HOST, () => {
+  process.stdout.write(`Server listening at -> http://${SERVER_HOST}:${SERVER_PORT}\n`);
+});
+
+module.exports = server;
