cff48b0b097505133193fe9ad49b60e1fa4dd9e5
zakariasan <elitziko@gmail.com>
2024-07-20 19:58:49 +0100

    full server add

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -1,12 +1,15 @@
-import AppController from '../controllers/AppController';
-import StudentsController from '../controllers/StudentsController';
+const express = require('express');
 
+const router = require('./routes/index');
 
-const mapRoutes = (app) => {
-  app.get('/', AppController.getHomepage);
-  app.get('/students', StudentsController.getAllStudents);
-  app.get('/students/:major', StudentsController.getAllStudentsByMajor);
-};
+const app = express();
+const PORT = 1245;
 
-export default mapRoutes;
-module.exports = mapRoutes;
+app.use('/', router);
+app.use('/students', router);
+app.use('/students/:major', router);
+
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT ${PORT}`);
+});
+export default app;
