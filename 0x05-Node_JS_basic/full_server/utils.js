6b9ef99030c7c21d62a4d8a34a549a328d90ac4b
zakariasan <elitziko@gmail.com>
2024-07-20 20:08:00 +0100

    add: utils

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,49 +1,26 @@
 import fs from 'fs/promises';
 
-/**
- * Reads the data of students in a CSV data file.
- * @param {string} filePath - The path to the database file.
- * @returns {Promise<object>} - A promise that resolves to an object of arrays of student first names per field.
- */
-const readDatabase = async (filePath) => {
-  if (!filePath) {
-    throw new Error('Cannot load the database');
-  }
-
-  try {
-    const data = await fs.readFile(filePath, 'utf-8');
-    const lines = data.trim().split('\n');
-    const headers = lines[0].split(',');
-    const fields = headers.slice(0, -1);
-    const groups = {};
-
-    for (const line of lines.slice(1)) {
-      const values = line.split(',');
-      const student = values.slice(0, -1);
-      const field = values[values.length - 1];
-
-      if (!groups[field]) {
-        groups[field] = [];
+module.exports = function readDatabase(filePath) {
+  const students = {};
+  return new Promise((resolve, reject) => {
+    fs(filePath, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        const lines = data.toString().split('\n');
+        const noHeader = lines.slice(1);
+        for (let i = 0; i < noHeader.length; i += 1) {
+          if (noHeader[i]) {
+            const field = noHeader[i].toString().split(',');
+            if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+              students[field[3]].push(field[0]);
+            } else {
+              students[field[3]] = [field[0]];
+            }
+          }
+        }
+        resolve(students);
       }
-
-      const studentEntry = fields.reduce((acc, field, index) => {
-        acc[field] = student[index];
-        return acc;
-      }, {});
-
-      groups[field].push(studentEntry);
-    }
-
-    const result = {};
-    for (const [field, students] of Object.entries(groups)) {
-      result[field] = students.map(student => student.firstname);
-    }
-
-    return result;
-  } catch (error) {
-    throw new Error('Cannot load the database');
-  }
+    });
+  });
 };
-
-export default readDatabase;
-
